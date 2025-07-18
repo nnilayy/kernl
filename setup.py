e21f20fa5b82bf75328401d4157f36ce266076a9
Nilay Bhatnagar <nnilayy.work@gmail.com>
2025-07-18 16:39:30 +0530

    Update lib version v0.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="kernl",
-    version="0.2.0",
+    version="0.3.0",
     author="Nilay Kumar Bhatnagar",
     author_email="nnilayy.work@email.com",
     description="To be Updated",
